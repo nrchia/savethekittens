2d3c6294eb40f29de839cdf1ba834fab03ee63fc
Dana Chiavoni <nicholaschiavoni@nicholass-macbook-pro.local>
2017-05-31 13:39:29 -0400

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Nicholas Chiavoni on 5/31/17.
+//  Copyright © 2017 ChiavoniCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
